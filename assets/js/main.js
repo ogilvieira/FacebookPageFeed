484a76d9105700ed10502be4bd33ea7c69609257
Gil Vieira <ogilvieira@gmail.com>
2016-07-06 20:03:01 -0300

    adjusts

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,34 @@ var fbFeed = new FacebookPageFeed({
     pagename : 'FacebookforDevelopers',
     format: 'html',
     feedlimit : 5,
+    template: function(page, post){
+
+      var postBody = '';
+      postBody = '{{cover}}{{text}}';
+      postBody = postBody.replace("{{cover}}", '<a href="'+post.link+'" target="_blank"><img src="'+post.attachments.data[0].media.image.src+'" class="img-responsive"></a>');
+      postBody = postBody.replace("{{text}}", '<p class="card-text">'+privateObj.urlify(post.message)+'</p>');
+
+      var tpl = '\
+        <div class="card card-block">\
+          <div class="panel-heading"">\
+            <div class="media">\
+              <div class="media-left">\
+                <a href="'+page.link+'" target="_blank"><img src="'+page.avatar+'"></a>\
+              </div>\
+              <div class="media-body">\
+                <h4 class="media-heading"><a href="'+page.link+'" target="_blank">'+page.name+'</a><br><small>'+page.likes+' likes</small>\</h4>\
+                <a href="'+post.link+'" target="_blank" class="date">'+post.created_time+'</a>\
+              </div>\
+            </div>\
+          </div>\
+          <div class="panel-body">'+postBody+'</div>\
+          <div class="panel-footer">\
+            <a href="'+post.link+'" class="label label-danger" target="_blank">'+((post.likes) ? post.likes.data.length : "0")+' likes</a>\
+          </div>\
+        </div>';
+
+      return tpl;
+    },
     onLoad: function(res, format, data){
       if(data && data.cover){
         $('.jumbotron').css({'background-image' : 'url(\''+data.cover.source+'\')'});
--- src/FacebookPageFeed.js
+++ src/FacebookPageFeed.js
@@ -9,31 +9,7 @@ var FacebookPageFeed = (function(defaultConfig){
 		feedlimit	: 10,
 		format	: 'json', //json, html
 		template: function(page, post){
-
-			var postBody = '';
-			postBody = '{{cover}}{{text}}';
-			postBody = postBody.replace("{{cover}}", '<a href="'+post.link+'" target="_blank"><img src="'+post.attachments.data[0].media.image.src+'" class="img-responsive"></a>');
-			postBody = postBody.replace("{{text}}", '<p class="card-text">'+privateObj.urlify(post.message)+'</p>');
-
-			var tpl = '\
-				<div class="card card-block">\
-					<div class="panel-heading"">\
-						<div class="media">\
-							<div class="media-left">\
-								<a href="'+page.link+'" target="_blank"><img src="'+page.avatar+'"></a>\
-							</div>\
-							<div class="media-body">\
-								<h4 class="media-heading"><a href="'+page.link+'" target="_blank">'+page.name+'</a><br><small>'+page.likes+' likes</small>\</h4>\
-								<a href="'+post.link+'" target="_blank" class="date">'+post.created_time+'</a>\
-							</div>\
-						</div>\
-					</div>\
-					<div class="panel-body">'+postBody+'</div>\
-					<div class="panel-footer">\
-						<a href="'+post.link+'" class="label label-danger" target="_blank">'+((post.likes) ? post.likes.data.length : "0")+' likes</a>\
-					</div>\
-				</div>';
-
+			var tpl = "<div>"+post.message+" - "+((post.likes) ? post.likes.data.length : "0")+"</div>";
 			return tpl;
 		},
 		onLoad: function(res){
